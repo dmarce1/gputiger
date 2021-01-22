de67334d54c53898a1fdf98976e199256be8036a
Dominic Marcello <dmarce504@gmail.com>
2021-01-21 18:47:40 -0600

    Adjusted big sort depth

--- src/main.cu
+++ src/main.cu
@@ -276,8 +276,8 @@ int main() {
 
 	size_t stack_size;
 	size_t desired_stack_size = 4 * 1024;
-	size_t rlimit = 4;
-	size_t heapsize = 8 * 1024 * 1024;
+	size_t rlimit = 7;
+	size_t heapsize = 4 * 1024 * 1024;
 	CUDA_CHECK(cudaDeviceSetLimit(cudaLimitDevRuntimeSyncDepth, rlimit));
 	CUDA_CHECK(cudaDeviceGetLimit(&rlimit, cudaLimitDevRuntimeSyncDepth));
 	printf("CUDA recursion limit = %li\n", rlimit);
@@ -307,7 +307,7 @@ int main() {
 	params.box_size = 1000;
 	//	params.box_size = 613.0 / 2160.0 * params.Ngrid;
 	params.nout = 64;
-	params.max_kernel_depth =  3;
+	params.max_kernel_depth =  6;
 	params.parts_per_bucket = 128;
 	params.opening_crit = 0.7;
 	params.nparts = params.Ngrid * params.Ngrid * params.Ngrid;
